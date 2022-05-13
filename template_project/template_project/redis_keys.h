a019da7e71f238ef444c5a86d0df2e14d2235ee6
SamRod2023 <93302670+SamRod2023@users.noreply.github.com>
2022-05-12 17:12:03 -0700

    Add files via upload

---
+++ template_project/template_project/redis_keys.h
@@ -0,0 +1,14 @@
+/**
+ * @file redis_keys.h
+ * @brief Contains all redis keys for simulation and control.
+ * 
+ */
+
+const std::string JOINT_ANGLES_KEY = "sai2::sim::panda::sensors::q";
+const std::string JOINT_VELOCITIES_KEY = "sai2::sim::panda::sensors::dq";
+const std::string JOINT_TORQUES_COMMANDED_KEY = "sai2::sim::panda::actuators::fgc";
+const std::string CONTROLLER_RUNNING_KEY = "sai2::sim::panda::controller";
+
+const std::string OBJECT_POS_KEY = "sai2::sim::cup::actuator::pos";
+const std::string HAPTIC_POS_KEY = "sai2::ChaiHapticDevice::device0::sensors::current_position";
+
